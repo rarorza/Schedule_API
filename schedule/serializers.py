0f1fa621672d24847b8864c1e9f8537b685d45e8
Rafael Rosa <rarorza@proton.me>
2023-09-14 18:10:48 -0300

    feat: added serializer validation to date_time

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from schedule.models import Scheduling
@@ -13,3 +14,17 @@ class SchedulingSerializer(serializers.ModelSerializer):
             "email",
             "phone",
         ]
+
+    def validate_date_time(self, value):
+        """Validate if data_time is not in the past
+
+        Keyword arguments:
+        value -- body request data
+        Return: datetime obj
+        """
+
+        if value < timezone.now():
+            raise serializers.ValidationError(
+                "Scheduling cannot be done in the past",
+            )
+        return value
